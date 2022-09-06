fea0f570574173e7df8e0ee6ffbb28d7805aa35f
Roman671975 <roman671975@gmail.com>
2022-09-06 21:47:12 +0300

    дз к 9-му семинару

---
+++ 9/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+int m = 1;
+int n = 10;
+int NaturalТnumber(int m, int n)
+{
+    if (m > n && n > 0)
+    {
+        return NaturalТnumber(n,m);
+    }
+    else if (m > 0 && m < n)
+    {
+        System.Console.Write(m + " ");
+        return NaturalТnumber(m + 1, n);
+    }
+    else
+    {
+        return m;
+    }
+}
+NaturalТnumber(m, n);
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = 1;
+int n = 15;
+int NaturalТnumber(int m, int n)
+{
+    int sum = 0;
+    for (int i = m; i <= n; i++)
+    {
+        sum +=i;
+    }
+    return sum;
+}
+System.Console.WriteLine(NaturalТnumber(m, n));
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int m = 2;
+int n = 3;
+int FunctionAkkerman(int m, int n)
+{
+  if (m == 0)
+    return m + 1;
+  else
+    if ((m != 0) && (m == 0))
+      return FunctionAkkerman(m - 1, 1);
+    else
+      return FunctionAkkerman(m - 1, FunctionAkkerman(m, m - 1));
+}
+Console.WriteLine(FunctionAkkerman(m,n));
